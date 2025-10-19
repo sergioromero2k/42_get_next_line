f17896b79f4ea37424997d1e9681870eaf6deed9
Sergio Alejandro <sergioromero2k@gmail.com>
2025-10-19 21:52:34 +0200

    Estudiando mas problemas extras para entender mas la funcion

--- Teoria/file_descriptor/ex2_cp_files.c
+++ Teoria/file_descriptor/ex2_cp_files.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+
+# define BUFFER_SIZE 1024
 
 int main(int argc, char **argv)
 {
-	int fd_read;
-	int fd_write;
+	int 	fd_read;
+	int 	fd_write;
+	char	str[BUFFER_SIZE];
 
 	if (argc != 3)
-		return 1;
+		return EXIT_FAILURE;
 
 	fd_read=open(argv[1],O_RDONLY);
 	if (fd_read == -1)
-		return 1;
+		return EXIT_FAILURE;
 
 	fd_write=open(argv[2],O_WRONLY | O_CREAT, 0664);
 	if (fd_write == -1)
-		return 1;
+		return EXIT_FAILURE;
+
+	while((n_bytes=read(fd,str,sizeof(str)))>0)
+	{
+		if(write(fd_write,str,n_bytes)==-1)
+		{
+			perror();
+		}
+	}
 
 
