e93227e9b8d22a66952f88d90304b9d63de9e85e
Sergio Alejandro <sergioromero2k@gmail.com>
2025-10-25 15:15:00 +0200

    Terninando copy archivos

--- Teoria/file_descriptor/Practice/ex1_fd_write.c
+++ Teoria/file_descriptor/Practice/ex1_fd_write.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int	ft_strlen(char *arr)
@@ -7,7 +8,9 @@ int	ft_strlen(char *arr)
 	int	i;
 
 	i = 0;
-	while (!arr)
+	if (!arr)
+		return (EXIT_FAILURE);
+	while (*arr)
 	{
 		i++;
 		arr++;
@@ -15,20 +18,31 @@ int	ft_strlen(char *arr)
 	return (i);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*arr;
 
-	arr = "Hola, mundo con fd!";
+	arr = "Hola mundo con fd!";
+	if (argc != 2)
+		return (EXIT_FAILURE);
 	// ssize_t bytes_leidos;
-	fd = open("salida.txt", O_WRONLY | O_CREAT, 0664);
+	fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
 		perror("Ha pasado un error al crear el archivo");
 		return (1);
 	}
-	write(fd, arr, ft_strlen(arr));
-	close(fd);
+	if ((write(fd, arr, ft_strlen(arr))) == -1)
+	{
+		perror("Error al escribir en el archivo");
+		close(fd);
+		return (EXIT_FAILURE);
+	}
+	if (close(fd) == -1)
+	{
+		perror("Error al cerrar el archivo");
+		return (EXIT_FAILURE);
+	}
 	return (0);
 }
--- Teoria/file_descriptor/Practice/ex2_cp_files.c
+++ Teoria/file_descriptor/Practice/ex2_cp_files.c
@@ -1,33 +1,55 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-# define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int 	fd_read;
-	int 	fd_write;
-	char	str[BUFFER_SIZE];
+	int fd_read;
+	int fd_write;
+	char str[BUFFER_SIZE];
+	ssize_t n_bytes;
 
 	if (argc != 3)
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 
-	fd_read=open(argv[1],O_RDONLY);
+	fd_read = open(argv[1], O_RDONLY);
 	if (fd_read == -1)
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 
-	fd_write=open(argv[2],O_WRONLY | O_CREAT, 0664);
+	fd_write = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
 	if (fd_write == -1)
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 
-	while((n_bytes=read(fd,str,sizeof(str)))>0)
+	while ((n_bytes = read(fd_read, str, sizeof(str))) > 0)
 	{
-		if(write(fd_write,str,n_bytes)==-1)
+		if (write(fd_write, str, n_bytes) == -1)
 		{
-			perror();
+			perror("Error al escribir archivo");
+			close(fd_read);
+			close(fd_write);
+			return (EXIT_FAILURE);
 		}
 	}
-
-
+	if (n_bytes == -1)
+	{
+		perror("Error de lectura en el archivo de origen");
+		close(fd_read);
+		close(fd_write);
+		return (EXIT_FAILURE);
+	}
+	if (close(fd_read) == -1)
+	{
+		perror("Error al cerrar el archivo de lectura");
+		return (EXIT_FAILURE);
+	}
+	if (close(fd_write) == -1)
+	{
+		perror("Error al cerrar de escritura");
+		return (EXIT_FAILURE);
+	}
+	return (0);
+}
