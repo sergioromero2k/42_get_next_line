c864b4d6a1704e8f00400b8f5b05d520e2fd5080
Sergio Alejandro <sergioromero2k@gmail.com>
2025-10-28 18:05:06 +0100

    Problema 2 - Contador líneas

--- Teoria/file_descriptor/EOF/Practice/ex2_count_lines.c
+++ Teoria/file_descriptor/EOF/Practice/ex2_count_lines.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
-
 #define BUFFER_SIZE 100
-
-// Si lo hiciste asi, solo cuenta cuántas veces read() leyó algo.
-// Cada vez que read() devuelve algo (puede ser una o varias líneas, o incluso una línea incompleta).
-// Pero read() no garantiza leer justo hasta un \n — puede leer trozos arbitrarios de datos según el tamaño del búfer o cómo el sistema entregue la entrada.
 
 int	main(void)
 {
 	char	buffer[BUFFER_SIZE];
 	ssize_t	n_lineas;
 	ssize_t	n_bytes;
+	int		i;
+	ssize_t	last_bytes;
 
 	n_lineas = 0;
 	while ((n_bytes = read(0, buffer, BUFFER_SIZE)) > 0)
 	{
+		last_bytes = n_bytes;
 		write(1, buffer, n_bytes);
-		n_lineas += 1;
+		i = 0;
+		while (i < n_bytes)
+		{
+			if (buffer[i] == '\n')
+				n_lineas++;
+			i++;
+		}
 	}
-		printf("Nº líneas: %ld\n", n_lineas);
+	if (last_bytes > 0 && buffer[last_bytes - 1] != '\n')
+		n_lineas++;
+	printf("Nº líneas: %ld\n", n_lineas);
 	return (0);
 }
