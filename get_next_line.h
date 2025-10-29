b96d44ef6abe069898de7d298829955d3bd24b24
Sergio Alejandro <sergioromero2k@gmail.com>
2025-10-29 21:07:47 +0100

    Subiendo funciones necesarias

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,14 @@
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 21:05:20 by sergio-alej       #+#    #+#             */
-/*   Updated: 2025/10/18 21:05:20 by sergio-alej      ###   ########.fr       */
+/*   Updated: 2025/10/29 21:04:24 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,37 @@
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 21:05:07 by sergio-alej       #+#    #+#             */
-/*   Updated: 2025/10/18 21:05:09 by sergio-alej      ###   ########.fr       */
+/*   Updated: 2025/10/29 21:03:51 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char *str;
+
+	str = (char *)s;
+	while (*str)
+	{
+		if (*str == (unsigned char)c)
+			return (str);
+		str++;
+	}
+	if ((unsigned char)c == '\0')
+		return (str);
+	return (NULL);
+}
